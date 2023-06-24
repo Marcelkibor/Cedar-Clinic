08bc92f4e2dcc52c8ef9d194462f5abb872496f7
Marcelkibor <kibormarcel90@gmail.com>
2023-06-24 21:54:48 +0300

    all-services responsive

--- src/components/Services/AllServices.tsx
+++ src/components/Services/AllServices.tsx
@@ -7,13 +7,17 @@ import Pagination from './Pagination';
 
 const AllServices = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [servicePerPage, setServicePerPage] = useState(2);
+  const [servicePerPage, setServicePerPage] = useState(4);
 
   const services = [
     { id: 1, title: 'Radiology', description: 'New radiology services' },
-    { id: 2, title: 'Histo', description: 'New radiology services' },
+    { id: 2, title: 'Oncology Services', description: 'Our Comprehensive Oncology Centre is fully equipped with the latest state of the art equipment for cancer diagnosis and treatment.' },
     { id: 3, title: 'Histopathology', description: 'New radiology services' },
     { id: 4, title: 'Palliative Care', description: 'New radiology services' },
+    { id: 5, title: 'Radiology', description: 'New radiology services' },
+    { id: 6, title: 'Oncology Services', description: 'Our Comprehensive Oncology Centre is fully equipped with the latest state of the art equipment for cancer diagnosis and treatment.' },
+    { id: 7, title: 'Histopathology', description: 'New radiology services' },
+    { id: 8, title: 'Palliative Care', description: 'New radiology services' },
   ];
 
   const lastServiceIndex = currentPage * servicePerPage;
@@ -25,10 +29,14 @@ const AllServices = () => {
       <Navigation />
       <div style={{ display: 'flex', width: '100vw', height: '20vh', background: '#00b894', position: 'relative', top: '70px' }}>
         <BsArrowRightCircleFill style={{ top: '25%', left: '7%', color: 'white', position: 'absolute' }} size={50} />
-        <h2 style={{ top: '25%', left: '12%', color: 'white', position: 'absolute' }}>All Services</h2>
+        <div style={{display:'flex', position:'absolute',top:'28%',left:'12%',outline:'no-outline'}}>
+        <a href='/'>Home</a>
+        <a style = {{marginLeft:'20px'}}href='/services'>All Services</a>
       </div>
+      </div>
+    
       <Row >
-        <Col className='all-sv-row' style={{ width: '25vw' }}>
+        <Col className='all-sv-banner' style={{ width: '25vw' }}>
           <div style={{ width: '370px', height: '60px', background: '#218c74', display: 'flex', alignItems: 'center' }}>
             <h5 style={{ marginLeft: '20px' }}>All Services</h5>
           </div>
@@ -39,10 +47,10 @@ const AllServices = () => {
           ))}
         </Col>
         <Col>
-          <Row style = {{marginLeft:'-30%'}}>
+          <Row className = 'all-sv-row'>
             {displayServices.map(service => (
-              <Col key={service.id} xs={6} style={{ marginBottom: '10px' }}>
-                <Card style={{ width: '350px' }}>
+              <Col key={service.id} style={{ marginBottom: '10px' }}>
+                <Card className = 'all-sv-card'>
                   <Card.Body>
                     <Card.Title>{service.title}</Card.Title>
                     <Card.Text>{service.description}</Card.Text>
