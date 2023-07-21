358e13b394d492f803a6be3f02ec42d2668f5a6a
Marcelkibor <kibormarcel90@gmail.com>
2023-07-22 02:41:42 +0300

    style team component

--- src/components/BulkItems/ClinicMembers.ts
+++ src/components/BulkItems/ClinicMembers.ts
@@ -1,16 +1,14 @@
 import Doc from '../../assets/doc.png'
 const clinicMembers = [
-{id:1,name:'Dr Jakait',src:Doc,title:'Head Doctor'},
-{id:1,name:'Dr Linda',src:Doc,title:'Doctor'},
-{id:1,name:'Allan Waswa',src:Doc,title:'Lab Technician'},
-{id:1,name:'Emily Wesonga',src:Doc,title:'Lab Technician'},
-{id:1,name:'Brian Chumba',src:Doc,title:'Accountant'},
-{id:1,name:'Lilian Wanjiku',src:Doc,title:'Accountant'},
-{id:1,name:'Cleophas Onyango',src:Doc,title:'Pharmacist'},
-{id:1,name:'Angela Wema',src:Doc,title:'Pharmacist'},
-{id:1,name:'John Doe',src:Doc,title:'Head Nurse'},
-{id:1,name:'Eliazar Magut',src:Doc,title:'Nurse'},
-{id:1,name:'Jenna Hassan',src:Doc,title:'Nurse'}
+{id:1,name:'Dr. Jakait',src:Doc,title:'Head Doctor',divColor:'#55efc4'},
+{id:2,name:'Dr. Linda',src:Doc,title:'Doctor',divColor:'#55efc4'},
+{id:3,name:'Mr. Allan Waswa',src:Doc,title:'Lab Technician',divColor:'#55efc4'},
+{id:4,name:'Mr. Brian Chumba',src:Doc,title:'Accountant',divColor:'#ffeaa7'},
+{id:5,name:'Ms. Lilian Wanjiku',src:Doc,title:'Accountant',divColor:'#ffeaa7'},
+{id:6,name:'Mr. Cleophas Onyango',src:Doc,title:'Pharmacist',divColor:'#ffeaa7'},
+{id:7,name:'Mr. John Doe',src:Doc,title:'Head Nurse',divColor:'#74b9ff'},
+{id:8,name:'Mr. Eliazar Magut',src:Doc,title:'Nurse',divColor:'#74b9ff'},
+{id:9,name:'Mrs. Jenna Hassan',src:Doc,title:'Nurse',divColor:'#74b9ff'}
 ]
 
 export default clinicMembers;
--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -1,6 +1,7 @@
 import { Card, Col, Row } from "react-bootstrap"
 import Navigation from "./Layouts/NavigationBar"
 import ServicesHeader from "./Services/ServicesHeader"
+import Doc from '../assets/doc.png'
 import clinicMembers from "./BulkItems/ClinicMembers"
 
 const OurTeam = () => {
@@ -8,16 +9,27 @@ const OurTeam = () => {
     <div>
         <Navigation/>
     <ServicesHeader/>
-    <Row style={{margin:'5% 0% 0% 0%'}}>
+    <Row style={{margin:'8% 0% 0% 0%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+    <div style={{width:'600px',height:'350px', textAlign:'center'}}>
+    <img src={Doc} style={{height:'100%',width:'100%'}}/>
+    <div style={{marginTop:'15px'}}>
+    <h4>Dr Jakait</h4>
+    <p>Head Doctor</p>
+    </div>
+    </div>
+    </Row>
+
+    <Row style={{margin:'10% 3% 0% 3%', textAlign:'center'}}>
         {clinicMembers.map((item:any,index:any)=>(
-            <Col lg = {4} md = {6} sm = {6}key={index}><Card>
-                <Card.Body>
+            <Col lg = {4} md = {6} sm = {6}key={index}><div style={{backgroundColor:`${item.divColor}`}}>
+                <div>
                     <img style = {{width:'100%',height:'100%'}}src={item.src}/>
-                </Card.Body>
-                <Card.Footer>
-                    <Card.Text>{item.name}<br></br>{item.title}</Card.Text>
-                </Card.Footer>
-                </Card></Col>
+                </div>
+                <div style={{paddingTop:'10px', backgroundColor:'white'}}>
+                    <h5>{item.name}</h5>
+                    <p>{item.title}</p>
+                </div>
+                </div></Col>
             ))}
         </Row>
     </div>
