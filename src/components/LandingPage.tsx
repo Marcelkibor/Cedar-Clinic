7cf26c07408198e7d1d7e781b6542318af261af9
Marcelkibor <kibormarcel90@gmail.com>
2023-08-03 21:45:23 +0300

    refactor homeBg

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,25 +1,33 @@
 import { Col, Row } from 'react-bootstrap';
+import Doc from '../assets/doc.png'
 import Img from '../assets/doc.png';
 import { FaClinicMedical, FaPills } from 'react-icons/fa';
-import { Carousel } from 'react-responsive-carousel';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import BgLanding from './DataFiles/BgLanding';
 const LandingPage = () => {
-    return (
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed:1000,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }; 
+  return (
   <div>
     <Row>
-    <Carousel>
-      <div>
-          <img src="assets/1.jpeg" />
-          <p className="legend">Legend 1</p>
-      </div>
-      <div>
-          <img src="assets/2.jpeg" />
-          <p className="legend">Legend 2</p>
-      </div>
-      <div>
-          <img src="assets/3.jpeg" />
-          <p className="legend">Legend 3</p>
-      </div>
-  </Carousel>
+    <Slider {...settings}>
+    {BgLanding.map((bg)=>(
+        <Col style={{height:'100vh'}} lg={1} md={6} sm={6} key={bg.id}>
+          <div style={{position:'relative',height:'100%',width:'100%'}}>
+          <img style={{height:'100%',width:'100%'}} src={bg.src}/>
+          </div>
+        </Col>
+    ))}
+  </Slider>
     </Row>
     </div>
     )
