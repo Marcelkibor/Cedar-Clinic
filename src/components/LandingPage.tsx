63c785f939feb36b910cbbbc414ad412e7adac03
Marcelkibor <kibormarcel90@gmail.com>
2023-08-03 22:51:24 +0300

    auto play custom hook

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,35 +1,58 @@
+import React, { useRef, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import Doc from '../assets/doc.png'
-import Img from '../assets/doc.png';
 import { FaClinicMedical, FaPills } from 'react-icons/fa';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import BgLanding from './DataFiles/BgLanding';
+
 const LandingPage = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed:1000,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }; 
+  const sliderRef = useRef<Slider | null>(null);
+  const autoplayInterval = 8000;
+
+  useEffect(() => {
+    let intervalId: any;
+
+    const startAutoplay = () => {
+      intervalId = setInterval(() => {
+        if (sliderRef.current) {
+          sliderRef.current.slickNext();
+        }
+      }, autoplayInterval);
+    };
+
+    const stopAutoplay = () => {
+      clearInterval(intervalId);
+    };
+    startAutoplay();
+    return () => {
+      stopAutoplay();
+    };
+  }, []);
+
   return (
-  <div>
-    <Row>
-    <Slider {...settings}>
-    {BgLanding.map((bg)=>(
-        <Col style={{height:'100vh'}} lg={1} md={6} sm={6} key={bg.id}>
-          <div style={{position:'relative',height:'100%',width:'100%'}}>
-          <img style={{height:'100%',width:'100%'}} src={bg.src}/>
-          </div>
-        </Col>
-    ))}
-  </Slider>
-    </Row>
+    <div>
+      <Row>
+        <Slider
+          ref={(slider) => (sliderRef.current = slider)}
+          dots={false}
+          infinite={true}
+          autoplay={false}
+          speed={1000}
+          slidesToShow={1}
+          slidesToScroll={1}
+        >
+          {BgLanding.map((bg) => (
+            <Col style={{ height: '100vh' }} lg={1} md={6} sm={6} key={bg.id}>
+              <div style={{ position: 'relative', height: '100%', width: '100%', marginTop:'70px' }}>
+                <img style={{ height: '100%', width: '100%' }} src={bg.src} alt={`Slide ${bg.id}`} />
+              </div>
+            </Col>
+          ))}
+        </Slider>
+      </Row>
     </div>
-    )
-  }
-  export default LandingPage
+  );
+};
+
+export default LandingPage;
