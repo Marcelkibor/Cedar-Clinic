b738472e0d06894dea7dc5591fc4e24ebfbb7cd8
Marcelkibor <kibormarcel90@gmail.com>
2023-08-29 11:28:36 +0300

    refactor landing

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,8 +5,8 @@ import BgLanding from './DataFiles/BgLanding';
 
 const LandingPage = () => {
   const sliderRef = useRef<Slider | null>(null);
-  const autoplayInterval = 8000;
-  const textDisplayDelay = 500; // Adjust the delay time as needed
+  const autoplayInterval = 6000;
+  const textDisplayDelay = 500; // delay time
   const [display, setDisplay] = useState(false);
   const [currentBgIndex, setCurrentBgIndex] = useState(0);
 
@@ -42,57 +42,37 @@ const LandingPage = () => {
 
   const showTextWithDelay = () => {
     setTimeout(() => {
-      setDisplay(true); // Show the text after the delay
+      setDisplay(true);
     }, textDisplayDelay);
   };
-
+const settings = {
+  infinite:true,
+  dots:false,
+  autoplay:false,
+  speed:1000,
+  slidesToShow:1,
+  slidesToScroll:1,
+  afterChange:handleAfterChange
+}
   return (
-    <div>
-      <Row>
-        <Slider
-          ref={(slider) => (sliderRef.current = slider)}
-          dots={false}
-          infinite={true}
-          autoplay={false}
-          speed={1000}
-          slidesToShow={1}
-          slidesToScroll={1}
-          afterChange={handleAfterChange}
-        >
-          {BgLanding.map((bg, index) => (
-            <Col style={{ height: '100vh' }} lg={1} md={6} sm={6} key={bg.id}>
-              <div
-                style={{
-                  position: 'relative',
-                  height: '100%',
-                  width: '100%',
-                  marginTop: '70px',
-                }}
-              >
-                <img
-                  style={{ height: '100%', width: '100%' }}
-                  src={bg.src}
-                  alt={`Slide ${bg.id}`}
-                />
-                {display && currentBgIndex === index && (
-                  <div
-                    className={`animated ${bg.animation}`}
-                    style={{
-                      position: 'absolute',
-                      top: '30%',
-                      left: '10%',
-                      width: '320px',
-                    }}
-                  >
-                    <h1 style={{ color: `${bg.color}` }}>{bg.text}</h1>
-                  </div>
-                )}
-              </div>
-            </Col>
-          ))}
-        </Slider>
-      </Row>
-    </div>
+<div>
+<Row>
+<Slider ref={(slider) => (sliderRef.current = slider)} {...settings}>
+  {BgLanding.map((bg, index) => (
+    <Col className = 'landing-col' lg={1} md={6} sm={6} key={bg.id}>
+      <div style={{position: 'relative',height: '100%',width: '100%',marginTop: '70px'}}>
+        <img style={{ height: '100%', width: '100%' }} src={bg.src} alt={`Slide ${bg.id}`}/>
+        {display && currentBgIndex === index && (
+          <div className={`animated ${bg.animation}`}style={{position: 'absolute',top: '30%', left: '10%',width: '320px'}}>
+            <h1 style={{ color: `${bg.color}` }}>{bg.text}</h1>
+          </div>
+        )}
+      </div>
+    </Col>
+  ))}
+    </Slider>
+</Row>
+  </div>
   );
 };
 
