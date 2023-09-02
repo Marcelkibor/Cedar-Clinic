3b8c0658454270d6124fd2811147ccf593b3c81c
Marcelkibor <kibormarcel90@gmail.com>
2023-09-02 20:45:42 +0300

    dynamic navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,45 +3,11 @@ import InsuranceProviders from "./InsuranceProviders"
 import LandingPage from "./LandingPage"
 import Navigation from "./Layouts/NavigationBar"
 import SliderServices from './Services/SliderServices'
-import { useEffect,useState } from "react"
-import Menu from "./Menu"
 const Home = () => {
-const [menuActive,setMenuActive] = useState(false)
-const[sroll,setScroll] = useState(false)
-useEffect(()=>{
-  if(window.innerWidth<=720){
-    setMenuActive(true);
-  }
-  else{
-    setMenuActive(false);
-  }
-  const isScrolled =()=>{
-    if(window.scrollY>20){
-      setScroll(true)
-    }
-    else{
-      setScroll(false);
-    }
-  }
-const getWidth = ()=>{
-  const h = window.innerWidth
-  if(h<=720){
-setMenuActive(true)
-  }else{
-    setMenuActive(false)
-  }
-}
-window.addEventListener('resize',getWidth)
-window.addEventListener('scroll',isScrolled)
-return ()=>{
-  window.removeEventListener('resize',getWidth)
-  window.removeEventListener('scroll',isScrolled)
-}
-},[])
   return (
     <div>
         <section id='#navbar'>
-          {menuActive?<div><Menu/></div>:<Navigation/>}
+        <Navigation/>
         </section>
         <section id = "#home">
             <LandingPage/>
--- src/components/Layouts/NavigationBar.tsx
+++ src/components/Layouts/NavigationBar.tsx
@@ -17,47 +17,71 @@ const Navigation = () => {
   };
 
   useEffect(() => {
+    if(window.innerWidth<=720){
+      setMenuActive(true)
+    }
+    else{
+      setMenuActive(false);
+    }
+    const handleResize =()=>{
+      if(window.innerWidth<=720){
+        setMenuActive(true)
+      }
+      else{
+        setMenuActive(false);
+      }
+    }
+
     const handleScroll = () => {
       setScrolled(window.scrollY > 50);
     };
+    
     window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
   return (
-<div className={`navigation ${scrolled ? 'navigation-active' : 'navigation-inactive'}`}>
-<Navbar>
-  <Container>
-   <div>
-    <Nav className="me-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <div
-        className="about-us-wrapper"
-        onMouseLeave={() => setShowLinks(false)}
-      >
-        <Nav.Link
-          onMouseEnter={handleHover}
-        >
-          About Us
-        </Nav.Link>
-        <div>
-        {hoveredLink === 'About Us' && showLinks && (
-          <div className="additional-links">
-            <Nav.Link href="/about-us" onClick={handleLinkClick}>Who We Are</Nav.Link>
-            <Nav.Link href="/our-team" onClick={handleLinkClick}>Our Team</Nav.Link>
+    <>
+    {menuActive?
+    <Menu/>:
+    <div className={`navigation ${scrolled ? 'navigation-active' : 'navigation-inactive'}`}>
+    <Navbar>
+      <Container>
+       <div>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <div
+            className="about-us-wrapper"
+            onMouseLeave={() => setShowLinks(false)}
+          >
+            <Nav.Link
+              onMouseEnter={handleHover}
+            >
+              About Us
+            </Nav.Link>
+            <div>
+            {hoveredLink === 'About Us' && showLinks && (
+              <div className="additional-links">
+                <Nav.Link href="/about-us" onClick={handleLinkClick}>Who We Are</Nav.Link>
+                <Nav.Link href="/our-team" onClick={handleLinkClick}>Our Team</Nav.Link>
+              </div>
+            )}
+            </div>
+      
           </div>
-        )}
-        </div>
-  
-      </div>
-      <Nav.Link href="/services">Services</Nav.Link>
-      <Nav.Link href="/contacts">Contacts</Nav.Link>
-    </Nav>
-      </div>
-  </Container>
-</Navbar>
-</div>
+          <Nav.Link href="/services">Services</Nav.Link>
+          <Nav.Link href="/contacts">Contacts</Nav.Link>
+        </Nav>
+          </div>
+      </Container>
+    </Navbar>
+    </div>
+  }
+    </>
+
   );
 };
 export default Navigation;
