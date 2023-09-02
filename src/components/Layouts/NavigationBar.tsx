a9d74d3a27243184097f902f5173dbf736b9f594
Marcelkibor <kibormarcel90@gmail.com>
2023-09-02 23:05:50 +0300

    refactor nav

--- src/components/Layouts/NavigationBar.tsx
+++ src/components/Layouts/NavigationBar.tsx
@@ -1,65 +1,97 @@
 import { useEffect, useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import Menu from '../Menu';
-
 const Navigation = () => {
-  const [menuActive, setMenuActive] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+  const [hoveredLink, setHoveredLink] = useState(null);
+  const [menuActive,setMenuActive] = useState(false)
+  const handleHover = (event:any) => {
+    const linkText = event.target.innerText;
+    setHoveredLink(linkText);
+    setShowLinks(true);
+  };
+
+  const handleLinkClick = () => {
+    setShowLinks(false);
+  };
 
   useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 720) {
-        setMenuActive(true);
-      } else {
+    if(window.innerWidth<=720){
+      setMenuActive(true)
+    }
+    else{
+      setMenuActive(false);
+    }
+    const handleResize =()=>{
+      if(window.innerWidth<=720){
+        setMenuActive(true)
+      }
+      else{
         setMenuActive(false);
       }
-    };
+    }
 
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+    
+    window.addEventListener('scroll', handleScroll);
     window.addEventListener('resize', handleResize);
-    handleResize(); // Initial check on component mount
     return () => {
+      window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-
   return (
     <>
-      {menuActive ? (
-        <div
-          style={{
-            height: 'fit-content',
-            position: 'fixed',
-            zIndex: 9999,
-            width: '100%',
-            top: 0,
-            backgroundColor: 'white',
-          }}
-        >
-          <Menu />
-        </div>
-      ) : (
-        <div className="navigation">
-          <Navbar>
-            <Container>
-              <div>
-                <Nav className="me-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <div className="about-us-wrapper">
-                    <Nav.Link>About Us</Nav.Link>
-                    <div className="additional-links">
-                      <Nav.Link href="/about-us">Who We Are</Nav.Link>
-                      <Nav.Link href="/our-team">Our Team</Nav.Link>
-                    </div>
-                  </div>
-                  <Nav.Link href="/services">Services</Nav.Link>
-                  <Nav.Link href="/contacts">Contacts</Nav.Link>
-                </Nav>
+    {menuActive?
+    <div style={{
+      height: 'fit-content',
+      position: 'fixed',
+      zIndex: 9999,
+      width: '100%',
+      top: 0,
+      backgroundColor: 'white',
+    }}>
+ <Menu/>
+    </div>
+   :
+    <div className={`navigation ${scrolled ? 'navigation-active' : 'navigation-inactive'}`}>
+    <Navbar>
+      <Container>
+       <div>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <div
+            className="about-us-wrapper"
+            onMouseLeave={() => setShowLinks(false)}
+          >
+            <Nav.Link
+              onMouseEnter={handleHover}
+            >
+              About Us
+            </Nav.Link>
+            <div>
+            {hoveredLink === 'About Us' && showLinks && (
+              <div className="additional-links">
+                <Nav.Link href="/about-us" onClick={handleLinkClick}>Who We Are</Nav.Link>
+                <Nav.Link href="/our-team" onClick={handleLinkClick}>Our Team</Nav.Link>
               </div>
-            </Container>
-          </Navbar>
-        </div>
-      )}
+            )}
+            </div>
+      
+          </div>
+          <Nav.Link href="/services">Services</Nav.Link>
+          <Nav.Link href="/contacts">Contacts</Nav.Link>
+        </Nav>
+          </div>
+      </Container>
+    </Navbar>
+    </div>
+  }
     </>
+
   );
 };
-
 export default Navigation;
