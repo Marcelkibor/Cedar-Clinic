337537cb9cc09eb630b91af07d73203c2495b2b6
Marcelkibor <kibormarcel90@gmail.com>
2023-09-03 10:42:51 +0300

    Nav dropdown options

--- src/components/NavbarItems.tsx
+++ src/components/NavbarItems.tsx
@@ -1,42 +1,53 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Nav } from "react-bootstrap";
+import { Nav, NavDropdown } from "react-bootstrap";
 
-const Navbar = (isToggled:any) => {
-  const items = ["Home", "Services", "About Us", "Contacts"];
+const items = [
+  { text: "Home", link: "/" },
+  { text: "Services", link: "/services" },
+  {
+    text: "About Us",
+    options: [
+      { text: "About Us", link: "/about-us" },
+      { text: "Our Team", link: "/our-team" },
+    ],
+  },
+  { text: "Contacts", link: "/contacts" },
+];
 
-  const navList = {
+const Navbar = () => {
+  const navListVariants = {
     visible: {
       opacity: 1,
       transition: {
         delayChildren: 0.2,
-        staggerChildren: 0.07
-      }
+        staggerChildren: 0.07,
+      },
     },
     hidden: {
       opacity: 0,
       transition: {
         staggerChildren: 0.05,
-        staggerDirection: -1
-      }
-    }
+        staggerDirection: -1,
+      },
+    },
   };
 
-  const navItem = {
+  const navItemVariants = {
     visible: {
       y: 0,
       opacity: 1,
       transition: {
-        y: { stiffness: 1000, velocity: -100 }
-      }
+        y: { stiffness: 1000, velocity: -100 },
+      },
     },
     hidden: {
       y: 50,
       opacity: 0,
       transition: {
-        y: { stiffness: 1000, velocity: -100 }
-      }
-    }
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    },
   };
 
   return (
@@ -46,44 +57,27 @@ const Navbar = (isToggled:any) => {
         initial="hidden"
         animate="visible"
         exit="hidden"
-        variants={navList}
+        variants={navListVariants}
       >
-      {items.map(item => {
-        if (item === 'About Us') {
-          return (
-            <motion.div
-              style={{ margin: '5% 0% 10% 20%', width: '100px' }}
-              key={item}
-            >
-              <Nav.Link href={`/about-us`}>{item}</Nav.Link>
-            </motion.div>
-          );
-        }if (item === 'Home') {
-          return (
-            <motion.div
-              style={{ margin: '5% 0% 10% 20%', width: '100px' }}
-              key={item}
-            >
-              <Nav.Link href={`/`}>{item}</Nav.Link>
-            </motion.div>
-          );
-        }if(item === 'Services'){
-          return (
-            <motion.div key={item}
-            style={{ margin: '5% 0% 10% 20%', width: '100px' }}>
-              <Nav.Link href={`/services`}>{item}</Nav.Link>
-            </motion.div>
-          );
-        }
-        if(item === 'Contacts'){
-          return (
-            <motion.div key={item}
-            style={{ margin: '5% 0% 10% 20%', width: '100px' }}>
-              <Nav.Link href={`/contacts`}>{item}</Nav.Link>
-            </motion.div>
-          );
-        }
-      })}
+        {items.map((item) => (
+          <motion.div
+            style={{ margin: '5% 0% 10% 20%', width: '100px' }}
+            key={item.text}
+            variants={navItemVariants}
+          >
+            {item.options ? (
+              <NavDropdown title={item.text}>
+                {item.options.map((option) => (
+                  <NavDropdown.Item key={option.text} href={option.link}>
+                    {option.text}
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+            ) : (
+              <Nav.Link href={item.link}>{item.text}</Nav.Link>
+            )}
+          </motion.div>
+        ))}
       </motion.div>
     </>
   );
