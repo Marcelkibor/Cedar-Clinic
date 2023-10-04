419becfd42c602cce4fadc3ef99735efb05bdf7c
Marcelkibor <kibormarcel90@gmail.com>
2023-10-04 12:51:36 +0300

    boxlink progress hover

--- src/components/BoxLinks.tsx
+++ src/components/BoxLinks.tsx
@@ -1,14 +1,37 @@
 import {Row,Col} from 'react-bootstrap';
 import BoxResource from './DataFiles/BoxFileResource';
+import { useAnimation } from 'framer-motion';
+import { useState } from 'react';
+import {motion} from 'framer-motion'
 const BoxLinks = () => {
+  const boxAnimate = useAnimation();
+  const [visible,setVisible] = useState(false);
+  const handleEnter =()=>{
+    setVisible(true);
+    boxAnimate.start({y:0})
+  }
+  const handleLeave=()=>{
+    setVisible(false);
+  }
   return (
     <div>
     <Row className='boxlinks'>
     {BoxResource.map((item:any)=>(
         <Col style = {{display:'flex',justifyContent:'center',alignItems:'center'}}key={item.id}>
-            <div className="boxlink-main">
-            <div style={{marginTop:'20%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <div onMouseEnter={handleEnter}onMouseLeave={handleLeave}style={{position:'relative'}}className="boxlink-main">
+            <div className='boxlink-text'>
+            <div>
             <h3 style={{color:'white'}}>{item.title}</h3>
+              </div>
+              {visible && 
+              <motion.div
+              initial={{y:-1}}
+              transition={{duration:0.3}}
+              animate={boxAnimate}
+              >
+              <p>Some Text</p>
+              </motion.div>
+              }
             </div>
             </div>
         </Col>
