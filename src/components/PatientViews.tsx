50dab619c507616688d036ea3a59ae9265f7e11f
Marcelkibor <kibormarcel90@gmail.com>
2023-10-06 16:17:12 +0300

    Patient review layout sylying

--- src/components/PatientViews.tsx
+++ src/components/PatientViews.tsx
@@ -1,18 +1,23 @@
 import Patient from '../assets/doc.png'
+import {Row,Col} from 'react-bootstrap';
 const PatientViews = () => {
   return (
-    <div className="review-main">
-        <div className="review-img">
+    <Row className='review-main'>
+      <Col sm={6} md={6} lg={6}>
+      <div className='review-img' >
             <img src={Patient} style={{margin:'5% 0% 0% -5%',width:'110%',height:'70%',justifyContent:'center',display:'flex',alignItems:'center'}}/>
         </div>
-        <div style={{maxWidth:'500px',margin:'6% 0% 0% 40%',textAlign:'center',color:'white'}}>
-                <p>
-                "I stayed in the hospital for 3 days and I felt at home. I was anxious before the surgery mostly because of the fear of the unknown, but I have had a smooth recovery post-surgery with no major complications.
-                 The nurses, doctors and the entire team at the hospital were very friendly and highly professional."
-                </p>
-                <p style={{fontWeight:'bold',fontStyle:'italic'}}>~ Allan Waswa</p>
+      </Col>
+       <Col sm={6} md={6} lg={6}>
+       <div className='review-text'>
+        <p>
+        "I stayed in the hospital for 3 days and I felt at home. I was anxious before the surgery mostly because of the fear of the unknown, but I have had a smooth recovery post-surgery with no major complications.
+          The nurses, doctors and the entire team at the hospital were very friendly and highly professional."
+        </p>
+        <p style={{fontWeight:'bold',fontStyle:'italic'}}>~ Allan Waswa</p>
             </div>
-    </div>
+       </Col>
+       </Row>
   )
 }
 
