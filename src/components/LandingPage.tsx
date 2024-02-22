2798927c3b0f15f22916f5d79278e00e4120caba
Marcelkibor <kibormarcel90@gmail.com>
2024-02-22 08:34:23 +0300

    style bg main

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -32,7 +32,7 @@ const LandingPage = () => {
       <Row className="g0"style={{width:'100%',height:'100%'}}>
         <Col>
         <div className='landing-tx' style={{color:"white"}}>
-    <p data-text='Cedar-Clinic' style={{fontWeight:'bolder',marginLeft:'30px',fontSize:'40px',fontFamily:'Merriweather serif'}}>Cedar Clinical Associates</p>
+    <p data-text='Cedar-Clinic' style={{fontWeight:'bolder',marginLeft:'30px',fontSize:'40px',fontFamily:'Merriweather serif'}}>Cedar Clinical <br></br>Associates</p>
     <p  style={{marginLeft:'30px',fontSize:'22px',fontFamily:'Merriweather serif'}}>Your Health</p>
     <p style={{marginTop:'-20px',marginLeft:'30px',fontSize:'22px',fontFamily:'Merriweather serif'}}>Is Our Priority</p>
   </div> 
